2f5624e99758931f5fceb6022dd4f6752d2e002d
Butemj Bat-Orshikh <40121569+buugiiman@users.noreply.github.com>
2026-09-23 17:05:52 +0900

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+        
+        Stack<Character> st = new Stack<>();
+        
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                st.push(')');
+            }
+
+            else if (ch == '[') {
+                st.push(']');
+            }
+
+            else if (ch == '{') {
+                st.push('}');
+            }
+
+            else if (st.isEmpty() || st.pop() != ch) {
+                return false;
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
